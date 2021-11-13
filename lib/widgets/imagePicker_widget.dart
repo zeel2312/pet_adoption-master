220b73ed9a159812f6951e9d2a3652c882271540
Rishi Patel <rishipatel232001@gmail.com>
2021-11-14 01:13:48 +0530

    initial commit

--- lib/widgets/imagePicker_widget.dart
+++ lib/widgets/imagePicker_widget.dart
@@ -1,10 +1,37 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ImagePickerWidget extends StatelessWidget {
+class ImagePickerWidget extends StatefulWidget {
+  @override
+  State<ImagePickerWidget> createState() => _ImagePickerWidgetState();
+}
+
+class _ImagePickerWidgetState extends State<ImagePickerWidget> {
+
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async{
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null){
+        _image = File(pickedFile.path);
+      }else{
+        print('No image selected');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+
+
+
     return Dialog(
       child: Column(
         children: [
@@ -17,28 +44,37 @@ class ImagePickerWidget extends StatelessWidget {
               style: TextStyle(color: Colors.black),
             ),
           ),
-          Column(
-            children: [
-              Container(
-                height: 120,
-                width: MediaQuery.of(context).size.width,
-                child: FittedBox(
-                  child: Icon(
-                    CupertinoIcons.photo_on_rectangle,
-                    color: Colors.grey,
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Container(
+                  height: 120,
+                  width: MediaQuery.of(context).size.width,
+                  child: FittedBox(
+                    child: _image == null ? Icon(
+                      CupertinoIcons.photo_on_rectangle,
+                      color: Colors.grey,
+                    ) : Image.file(_image),
                   ),
                 ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: NeumorphicButton(
-                      style: NeumorphicStyle(color: Theme.of(context).primaryColor),
+                Row(
+                  children: [
+                    Expanded(
+                      child: NeumorphicButton(
+                        onPressed: getImage,
+                          style: NeumorphicStyle(
+                              color: Theme.of(context).primaryColor),
+                          child: Text(
+                            'Upload image',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white),
+                          )),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           )
         ],
       ),
